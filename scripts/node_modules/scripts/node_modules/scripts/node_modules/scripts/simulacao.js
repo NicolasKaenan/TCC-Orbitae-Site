10b4ce68e4aa31c8e93cebc798067c677e41885f
Kaenan <nicolaskaenandev@gmail.com>
2024-11-16 00:49:16 -0300

    atualização

---
+++ scripts/node_modules/scripts/node_modules/scripts/node_modules/scripts/simulacao.js
@@ -0,0 +1,32 @@
+const corpos = require("./corpos")
+const sequelize = require("sequelize");
+const banco = require("./banco")
+const user = require("./user");
+
+var simulacao = banco.conexao.define(
+    "simulacao",
+    {
+        id:{
+            type:sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        id_user:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        },
+        nome:{
+            type:sequelize.STRING,
+            allowNull:false
+        },
+        cor:{
+            type:sequelize.STRING,
+            allowNull:false
+        }
+    },
+    { timestamps: false }
+)
+
+simulacao.hasMany(corpos.corpos,{foreignKey: 'id_simulacao'})
+
+module.exports = {simulacao}

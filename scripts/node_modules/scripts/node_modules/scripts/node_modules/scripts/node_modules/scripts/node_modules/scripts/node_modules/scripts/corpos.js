8935585de4eb84fe185e00972eb182880d9efc9e
Kaenan <nicolaskaenandev@gmail.com>
2024-11-16 00:37:55 -0300

    atualização

---
+++ scripts/node_modules/scripts/node_modules/scripts/node_modules/scripts/node_modules/scripts/node_modules/scripts/node_modules/scripts/corpos.js
@@ -0,0 +1,51 @@
+const sequelize = require("sequelize");
+const banco = require("./banco")
+
+var corpos = banco.conexao.define(
+    "corpos",
+    {
+        id:{
+            type:sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement:true
+        },
+        id_simulacao:{
+            type:sequelize.INTEGER.UNSIGNED,
+            allowNull:false
+        },
+        massa:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        cor:{
+            type:sequelize.STRING,
+            allowNull:false
+        },
+        position_x:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        position_y:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        position_z:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        velocidade_x:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        velocidade_y:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+        velocidade_z:{
+            type:sequelize.DOUBLE.UNSIGNED,
+            allowNull:false
+        },
+    }, { timestamps: false }
+)
+
+module.exports = { corpos };
